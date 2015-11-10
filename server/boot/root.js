14f69404533f0653046b5d3f6f8080927d8d77a5
Richard Pringle <rpringle@ca.ibm.com>
2015-11-10 09:37:50 -0500

    Testing caseSensitiveEmail settings on User model

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,4 +1,20 @@
 module.exports = function(server) {
+  var User = server.models.User;
+  User.settings.caseSensitiveEmail = false;
+  User.create({
+    email: 'Test@gmail.com', 
+    password: 'pooky'
+  }, function (err) {
+    User.findOne({
+      where: {
+        email: 'test@gmail.com'
+      } 
+    }, function (err, user) {
+      if (err) throw err;
+      console.log(user);
+    });
+  });
+
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
